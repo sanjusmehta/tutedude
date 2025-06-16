c6561c9650cbe45477fe19f59631f2487541837d
Sanjeev Mehta <sanjusmehta@yahoo.com>
2025-06-16 16:35:17 +0530

    Add files via upload

---
+++ Assignment02/Assignment02_Task01.py
@@ -0,0 +1,8 @@
+#CHECK NUMBER IS ODD OR EVEN
+
+number = int(input("Please enter a whole number : "))
+
+if (number % 2 == 0):
+    print(number, "is a Even Number")
+else:
+    print(number, "is a Odd number")
---
+++ Assignment02/Assignment02_Task02.py
@@ -0,0 +1,7 @@
+#SUM OF INTEGERS FROM 1 TO 50 USING A LOOP
+
+Total = 0
+for i in range(1,51):
+    Total += i
+
+print("The Sum of numbers from 1 to 50 is:", Total)
