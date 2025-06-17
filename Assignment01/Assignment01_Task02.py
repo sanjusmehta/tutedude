edc9db332de0d42bc74f333f47b7dd7bbe6ad569
Sanjeev Mehta <sanjusmehta@yahoo.com>
2025-06-17 16:08:33 +0530

    Create Assignment01_Task02.py

---
+++ Assignment01/Assignment01_Task02.py
@@ -0,0 +1,6 @@
+#TASK 2
+firstname = (input("Please Enter your First Name : "))
+
+lastname = (input("Please Enter your Last Name : "))
+
+print("Hi,",firstname,lastname+"!","Welcome to Python Programming.")
