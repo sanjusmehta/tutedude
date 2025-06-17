4dd0650b47cca27732c912682b46dca333e932d9
Sanjeev Mehta <sanjusmehta@yahoo.com>
2025-06-17 16:08:46 +0530

    Delete assignment01/Assignment01_Task02.py

--- assignment01/Assignment01_Task02.py
+++
@@ -1,6 +0,0 @@
-#TASK 2
-firstname = (input("Please Enter your First Name : "))
-
-lastname = (input("Please Enter your Last Name : "))
-
-print("Hi,",firstname,lastname+"!","Welcome to Python Programming.")
